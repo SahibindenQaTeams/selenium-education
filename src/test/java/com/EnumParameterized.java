73a6fdfefd8084a3ce980050368a45cea3eff099
Bahadir Ustun <Bahadir.Ustun@sahibinden.com>
2022-10-28 11:21:19 +0300

    education 5 session

--- src/test/java/com/EnumParameterized.java
+++ src/test/java/com/EnumParameterized.java
@@ -2,9 +2,24 @@ package com;
 
 public enum EnumParameterized {
 
-  AHMET,
-  MEHMET,
-  ALI,
-  VELI;
+  AHMET(1999,"Ankara"),
+  MEHMET(1994,"İstanbul"),
+  ALI(1993,"Çorum"),
+  VELI(1995,"Bolu");
 
+  int yil;
+  String il;
+
+  EnumParameterized(int yil, String il) {
+    this.yil = yil;
+    this.il = il;
+  }
+
+  public int getYil() {
+    return yil;
+  }
+
+  public String getIl() {
+    return il;
+  }
 }
--- src/test/java/com/ParameterizedTests.java
+++ src/test/java/com/ParameterizedTests.java
@@ -1,5 +1,7 @@
 package com;
 
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,8 +11,11 @@ import org.junit.jupiter.api.Tags;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.TestInstance.Lifecycle;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.EnumSource;
 import org.junit.jupiter.params.provider.EnumSource.Mode;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 @TestInstance(Lifecycle.PER_CLASS)
@@ -38,6 +43,7 @@ public class ParameterizedTests {
   }
 
 
+  //parametrik olarak A,B,C değerilerini teker teker aynı testi çalıştırıp değerlerini dinamik olarak kullanmamızı sağlar
   @Tag("hotfix")
   @Tag("S360")
   @ParameterizedTest
@@ -46,6 +52,7 @@ public class ParameterizedTests {
     System.out.println("ParameterizedTest methodu çalıştı - Value : "+ value);
   }
 
+  //parametrik olarak 1,2,3,4 değerilerini teker teker aynı testi çalıştırıp değerlerini dinamik olarak kullanmamızı sağlar
   @Tags({@Tag("ALISVERIS"),  @Tag("hotfix")})
   @ParameterizedTest
   @ValueSource(ints = {1,2,3,4})
@@ -53,12 +60,62 @@ public class ParameterizedTests {
     System.out.println("ParameterizedTest methodu çalıştı - Value : "+ value);
   }
 
+  //parametrik olarak Enum classı içerisinde tanımlanmış değerleri teker teker aynı testi çalıştırıp değerlerini dinamik olarak kullanmamızı sağlar
+  //Enum classı içerisinde o nesneye ait değerler verilebilir ve kullanılan nesnenin içerisinde bulunan değerler alınabilir
   @ParameterizedTest
-  @EnumSource(names = {"ALI","VELI"}, mode = Mode.EXCLUDE)
+  //Bütün değerleri alır
+  @EnumSource()
+  //ALI,VELI değerlerini alır sadece
+  //@EnumSource(names = {"ALI","VELI"}, mode = Mode.EXCLUDE)
+  //ALI,VELI değerleri hariç diğer değerleri alır
+  //@EnumSource(names = {"ALI","VELI"}, mode = Mode.INCLUDE)
+  //EnumParameterized enum class ını parametre olarak verdiğimizde @EnumSource sayesinde otomatik olarak o enum clasında bulunan değerleri kullandırır
   public void parameterizedTestForEnum(EnumParameterized value){
-    System.out.println("ParameterizedTest methodu çalıştı - EnumSource -> Value : "+ value);
+    System.out.println("ParameterizedTest methodu çalıştı - EnumSource -> Value : "+ value +" - Yıl -> "+ value.getYil() +" - İl -> "+value.getIl());
   }
 
+  //CsvFileSource parametre olarak resources değerini verdiğimizde proje içserisinde bulunan resources klasörü altında bulunan ilgili dosyayı okur
+  //csv file içerisinde bulunan sütün sayısı kadar bu testin methodunda parametre tanımlaması yapılmalıdır bu değerler satır satır okunur ve test satır uzunluğu boyunca çalıştırılır teker teker
+  @ParameterizedTest
+  @CsvFileSource(resources = "/csvFile.csv")
+  public void parameterizedTestForCsvFileSource(String name, Integer year){
+    System.out.println("ParameterizedTest methodu çalıştı - CsvFileSource -> Name : " + name +" - Year : "+year);
+  }
+
+  //Csv dosyası yerine csv tipinde değerler verip teker teker çalıştırmamızı sağlar
+  @ParameterizedTest
+  @CsvSource({
+      "apple,1",
+      "banana,3",
+      "orange,2",
+  })
+  public void parameterizedTestCsvSource(String fruit, int rank) {
+    System.out.println("ParameterizedTest methodu çalıştı - CsvSource -> Fruit : " + fruit +" - Rank : "+rank);
+  }
+
+  //MethodSource dinamik olarak değerler için bir method oluşturulup bu method içerisinde bulunan değeri return ettirip bu değerleri almamıza olanak tanır
+  //static olarak tanımlanan stringProvider methodunun ismini MethodSource a tanıtmamız gerekmektedir
+  @ParameterizedTest
+  @MethodSource("stringProvider")
+  public void parameterizedTestMethodSourceForString(String value) {
+    System.out.println("ParameterizedTest çalıştı - MethodSource for String -> " + value);
+  }
+
+  static Stream<String> stringProvider() {
+    return Stream.of("apple", "banana");
+  }
+
+  //MethodSource dinamik olarak değerler için bir method oluşturulup bu method içerisinde bulunan değeri return ettirip bu değerleri almamıza olanak tanır
+  //static olarak tanımlanan stringProvider methodunun ismini MethodSource a tanıtmamız gerekmektedir
+  @ParameterizedTest
+  @MethodSource("range")
+  void parameterizedTestMethodSourceForInteger(int value) {
+    System.out.println("ParameterizedTest çalıştı - MethodSource for Integer -> " + value);
+  }
+
+  static IntStream range() {
+    return IntStream.range(0, 20).skip(10);
+  }
 
 
 }
